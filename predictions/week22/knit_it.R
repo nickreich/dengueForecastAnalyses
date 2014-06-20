26f0b667e9da43419f968ff9d8556c9ee891da3e
Nicholas G Reich <nick@schoolph.umass.edu>
2014-06-20 16:24:49 -0400

    adding knitit file for week 22.

---
+++ predictions/week22/knit_it.R
@@ -0,0 +1,36 @@
+## dengue forecast analyses
+## Nicholas Reich
+## June 2014
+
+tic <- Sys.time()
+DATA_THRU_WEEK <- 22
+
+require(knitr)
+
+## set up paths and filenames
+pred_path <- file.path('~/Documents/code_versioned/denguePrediction/', 
+                       'dengueForecastAnalyses',
+                       'predictions',
+                       paste0('week', DATA_THRU_WEEK))
+setwd(pred_path)
+knitr_file_path <- file.path(pred_path, paste0('predictionReport_week', DATA_THRU_WEEK))
+
+## knit it
+knit(paste0(knitr_file_path, '.Rnw'))
+
+## tex it twice, for references to align
+system(paste0("pdflatex ", knitr_file_path, ".tex"))
+system(paste0("pdflatex ", knitr_file_path, ".tex"))
+
+
+knit('predictionReport_week20.Rnw'))
+
+## tex it twice, for references to align
+system(paste0("pdflatex ", knitr_file_path, ".tex"))
+system(paste0("pdflatex ", knitr_file_path, ".tex"))
+
+toc <- Sys.time()
+
+(toc-tic)
+
+
